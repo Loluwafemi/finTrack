313446325b8efcb2132c424aded1196fc37019cc
trojan-z <shokefunoa.20@student.funaab.edu.ng>
2025-06-28 12:25:42 +0100

    admin-updating

--- app/Admin/app/(auth)/signup.tsx
+++ app/Admin/app/(auth)/signup.tsx
@@ -1,10 +1,12 @@
-import { getRoute } from "@/src/constants/routes";
+import { Label } from "@react-navigation/elements";
 import { router } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { useColorScheme } from "../../lib/useColorScheme";
-import { COLORS } from "../../theme/colors";
-
+import { TextInput } from "react-native-gesture-handler";
+import '~/global.css';
+import { useColorScheme } from "~/lib/useColorScheme";
+import { getRoute } from "~/src/constants/routes";
+import { COLORS } from "~/theme/colors";
 
 /**
  * Dashboard Index Screen
@@ -14,7 +16,7 @@ import { COLORS } from "../../theme/colors";
  *
  * The screen is fully responsive and adapts to both light and dark themes.
  */
-export default function SignupIndex() {
+export default function AuthenticationIndex() {
   const { isDarkColorScheme } = useColorScheme();
   const currentColors = isDarkColorScheme ? COLORS.dark : COLORS.light;
 
@@ -32,39 +34,148 @@ export default function SignupIndex() {
         <Text
           style={[styles.comingSoonTitle, { color: currentColors.foreground }]}
         >
-          Signup Page
+          Get started
         </Text>
-        <Text
-          style={[
-            styles.comingSoonSubtitle,
-            { color: currentColors.textSecondary },
-          ]}
-        >
-          : EMAIL, PASSWORD, ACCESS KEY? (OPTIONAL: USED WHEN AN ACCOUNT NEED FIRST TIME ENTRY ACCESS FROM SUPER ADMIN,
-          Other details from user as well
-        </Text>
+          {/* Header Login */}
+        <View className="flex flex-col w-[350px]">
+          {/* Form for email password */}
+          <View className="flex flex-row items-center justify-center m-2">
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>First Name: </Label>
+              <TextInput 
+              className="px-[10px] py-[10px] border border-black w-full"
+              />
+
+            </View>
+
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Last Name: </Label>
+              <TextInput
+              className="px-[10px] w-full py-[10px] border border-black"
+              />
+
+            </View>
+          </View>
+    
+          <View className="flex flex-row items-center justify-center m-2">
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Email: </Label>
+              <TextInput inputMode="email" placeholder="joe@email.com" className="px-[10px] py-[10px] border border-black w-full"
+              />
+
+            </View>
+
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Phone Number: </Label>
+              <TextInput inputMode="text" placeholder="+234 xxxx xxxxx"
+              className="px-[10px] w-full py-[10px] border border-black"
+              />
+
+            </View>
+          </View>
+
+          <View className="flex flex-row items-center justify-center m-2">
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Password: </Label>
+              <TextInput inputMode="text" placeholder="*********" className="px-[10px] py-[10px] border border-black w-full"
+              />
+
+            </View>
+
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Confirm Password: </Label>
+              <TextInput placeholder="********" 
+              className="px-[10px] w-full py-[10px] border border-black"
+              />
+
+            </View>
+          </View>
+  
+          <View className="flex flex-row items-center justify-center m-2">
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Organization: </Label>
+              <TextInput inputMode="text" placeholder="Drop Down" className="px-[10px] py-[10px] border border-black w-full"
+              />
+
+            </View>
+
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Organization Invitation Key: </Label>
+              <TextInput placeholder="optional"
+              className="px-[10px] w-full py-[10px] border border-black"
+              />
+
+            </View>
+          </View>
+
+          {/* <View className="flex flex-row items-center justify-center m-2">
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Invitation Key: </Label>
+              <TextInput inputMode="text" placeholder="***********" className="px-[10px] py-[10px] border border-black w-full"
+              />
+
+            </View>
+
+            <View className="flex flex-col items-start mx-1 w-full">
+              <Label>Organization Name: </Label>
+              <TextInput placeholder="" 
+              className="px-[10px] w-full py-[10px] border border-black"
+              />
+
+            </View>
+          </View> */}
+
         <button
-        className="bg-black p-2 text-white rounded mt-4"
-        onClick={()=>{
-            router.navigate(getRoute('AUTH'));
-        }}>
-            Login
+            className="bg-white border-black border-2 p-2 text-black rounded mt-1"
+            onClick={()=>{
+                // router.navigate(getRoute('SIGNUP'));
+            }}>
+                Signup
         </button>
+
+
+
+        <View className="mt-4">
+        <Text>
+          Already have an account? Login now.
+        </Text>
+
+        <button
+            className="bg-black p-2 text-white rounded mt-1"
+            onClick={()=>{
+                router.navigate(getRoute('AUTH'));
+            }}>
+                Login
+        </button>
+
+        </View>
+        </View>
+
+
         {/* end here */}
       </View>
+
+      <button
+        className="bg-black p-2 text-white rounded mt-4"
+        onClick={()=>{
+            router.navigate(getRoute('DASHBOARD'));
+        }}>
+            Continue to dashboad
+        </button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
   },
   comingSoonContainer: {
-    flex: 1,
+    flex: 3,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 20,
+    paddingHorizontal: 30,
+    borderColor: 'black',
   },
   comingSoonTitle: {
     fontSize: 32,
---
+++ app/Admin/lib/server/server.tsx
@@ -0,0 +1,375 @@
+import * as SecureStore from 'expo-secure-store';
+
+
+interface userDataType {
+    organization: 'string',
+    bank_name: string,
+    bank_account_number: string,
+    bank_account_name: string,
+    firstname: string | null,
+    lastname: string | null,
+    // username: string | null,
+    email: string | null,
+    password: string | null
+}
+
+// export const backendORIGIN = "http://127.0.0.1:8080"
+export const backendORIGIN = "http://127.0.0.1:8080"
+export const api_origin_address = "127.0.0.1"
+export const AUTH_ORIGIN = process.env.ALLOWED_ORIGIN?.split(',')
+
+// api securities
+export const apiHeaders = new Map()
+
+apiHeaders.set("Authorization", "Bearer")
+apiHeaders.set("content-type", "application/json")
+apiHeaders.set("Access-Control-Allow-Origin", "http://192.168.43.107:8081")
+apiHeaders.set("Access-Control-Allow-Methods", "GET, POST")
+apiHeaders.set("Access-Control-Allow-Headers", "content-type, Authorization")
+
+
+export const storeData = async (key:any, value:any) => {
+    try {
+        await SecureStore.setItemAsync(key, value);
+        console.log("cookies has been set");
+        
+    } catch (e) {
+      // await CookieManager.setFromResponse(backendORIGIN, value);
+      console.log(e);
+      
+      console.log("Couldnt save data");
+      
+    }
+};
+
+export const getData = async (key:any) => {
+    let result = await SecureStore.getItemAsync(key);
+    if (result) {
+        console.log("🔐 Here's your value 🔐 \n" + result);
+        return result;
+      } else {
+        console.log('No values stored under that key.');
+      }
+};
+
+export const deleteCookie = async (key:any) => {
+    await SecureStore.deleteItemAsync(key);
+};
+
+
+// use this to manage all request
+export async function PostrequestHandler(path:{ url: null|string,  data: any }) {
+
+                // check if cookie is set
+            const cookie = await getData(api_origin_address)
+
+            if (cookie === undefined) return { status: false, message: "cookie not set. Try authenticate first" }    
+
+                apiHeaders.set('Cookie', cookie)
+
+            if (!path.url) return {
+                status: false,
+                message: "Path url to request not set",
+                data: null
+                }
+
+
+                // console.log(path.url);    
+
+            const request = await fetch(`${backendORIGIN}${path.url}`, {
+                headers: Object.fromEntries(apiHeaders.entries()),
+                method: 'POST',
+                body: JSON.stringify(path.data),
+                credentials: 'same-origin'
+                })
+
+
+            const responseClone = request.clone()
+            const sessionData = await responseClone.json()    
+
+            if (sessionData){
+                // redirect to dashboard.
+                return {
+                status: true,
+                message: "Valid Credential",
+                ...sessionData
+                }
+            }else{
+                return {
+                status: false,
+                message: "Invalid Credential",
+                data: null
+                }
+                }
+}
+
+
+export async function GetRequestHandler(path:{ url: null|string }) {
+    const cookie = await getData(api_origin_address)
+    if (cookie === undefined) return { status: false, message: "cookie not set. Try authenticate first" }  
+
+    apiHeaders.set('Cookie', cookie)
+
+    if (!path.url) return {
+        status: false,
+        message: "Path url to request not set",
+        data: null
+        }
+
+    const request = await fetch(`${backendORIGIN}${path.url}`, {
+        headers: Object.fromEntries(apiHeaders.entries()),
+        method: 'GET',
+        credentials: 'same-origin'
+    })
+
+
+    const responseClone = request.clone()
+    const session = await responseClone.json()   
+
+    if (session){
+        // redirect to dashboard.
+        return {
+            status: true,
+            message: "Valid Credential",
+            data: session
+        }
+    }else{
+        return {
+            status: false,
+            message: "Invalid Credential",
+            data: null
+        }
+    }
+}
+
+export async function signinREQUEST(data) {
+    
+    try {
+        
+        const request = await fetch(`${backendORIGIN}/api/auth/signin`, {
+            headers: Object.fromEntries(apiHeaders.entries()),
+            body: JSON.stringify(data),
+            method: 'POST',
+            credentials: 'same-origin'
+        })
+
+        console.log(request);
+
+        
+        const responseClone = request.clone()
+        
+
+        if (!responseClone.ok) {
+            return {
+
+                status: false,
+                message: "Invalid Credential. Response not ok",
+                cookies: null
+        }
+        }
+        
+        const {status, message } = await responseClone.json()
+        
+        if (status){
+            
+            const cookies = responseClone.headers.get('set-cookie')
+            // console.log(typeof cookies);
+            
+            await storeData(api_origin_address, cookies)
+            await getData(api_origin_address)   
+            
+            return {
+                status: true,
+                message: "Valid Credential " + message,
+            }
+        }else{
+            return {
+                status: false,
+                message: "Invalid Credential, response okay. Something went wrong",
+                cookies: null
+            }
+        }
+    
+    } catch (error) {
+        // console.log("error: ", error);
+        return {
+            status: false,
+            message: "Invalid Credential. Caught an error",
+            cookies: null
+        }
+        
+    }
+
+    
+};
+
+
+export async function SessionUser() {
+    
+    // check if cookie is set
+    const cookie = await getData(api_origin_address)
+    
+    if (cookie === undefined) return { status: false, message: "cookie not set. Try authenticate first" }    
+
+    apiHeaders.set('Cookie', cookie)
+
+    const request = await fetch(`${backendORIGIN}/api`, {
+        headers: Object.fromEntries(apiHeaders.entries()),
+        method: 'GET',
+        credentials: 'same-origin'
+    })
+    
+    const responseClone = request.clone()
+    const session = await responseClone.json()    
+
+    if (session){
+        // redirect to dashboard.
+        return {
+            status: true,
+            message: "Valid Credential",
+            data: session
+        }
+    }else{
+        return {
+            status: false,
+            message: "Invalid Credential",
+            data: null
+        }
+    }
+}
+
+
+
+export async function signouREQUEST(data?:any) {
+    // id is userid
+    // use session data to signout
+    let session = await SessionUser()
+
+    const cookie = await getData(api_origin_address)
+    
+    if (cookie === undefined) return { status: false, message: "cookie not set. Try authenticate first" }
+
+    apiHeaders.set('Cookie', cookie)
+
+    if (session.data == null) return { status: false, message: "Session does not exist" }
+
+    session = session.data
+    
+
+    const request = await fetch(`${backendORIGIN}/api/auth/signout`, {
+        headers: Object.fromEntries(apiHeaders.entries()),
+        body: JSON.stringify(session),
+        method: 'POST',
+        credentials: 'same-origin'
+    })
+
+    await deleteCookie(api_origin_address)
+    
+    const responseClone = request.clone()
+    const {status, message } = await responseClone.json()   
+    
+    if (status){
+        // redirect to dashboard.
+        // console.log("session has ended");
+        // console.log("session: ", session);
+        
+        
+        return {
+            status: true,
+            message: "Session has ended",
+        }
+    }else{
+        return {
+            status: false,
+            message: "Fail to end session. Something went wrong",
+        }
+    }
+    
+}
+
+export async function signupREQUEST(data:any) {
+    const cookie = await getData(api_origin_address)
+    
+    if (cookie !== undefined) return { status: false, message: "cookie already set. Try de-authenticate first" }
+
+
+    // breaking incoming data
+
+
+    const tailoredData:userDataType|any = {
+        firstname: data.firstname,
+        lastname: data.lastname,
+        email: data.email,
+        password: data.password,
+        bank_account_name: data.accountname,
+        bank_account_number: data.accountnumber,
+        bank_name: data.bank,
+        organization: data.organization
+    }
+
+    
+    const request = await fetch(`${backendORIGIN}/api/auth/signup`, {
+        headers: Object.fromEntries(apiHeaders.entries()),
+        body: JSON.stringify(tailoredData),
+        method: 'POST',
+        credentials: 'same-origin'
+    })
+
+    const responseClone = request.clone()
+    const {status, message} = await responseClone.json()    
+
+    if (status){     
+        
+        return {
+            status: true,
+            message: "Transaction successful",
+        }
+    }else{
+        return {
+            status: false,
+            message: message,
+            cookies: null
+        }
+    }
+    
+}
+
+
+export async function saveBudget(payload: {auth: any, data: any}) {
+
+    const cookie = await getData(api_origin_address)
+    
+    if (cookie === undefined) return { status: false, message: "cookie not set. Try authenticate first" }
+
+    // breaking incoming data
+    const request = await fetch(`${backendORIGIN}/api/service/add`, {
+        headers: Object.fromEntries(apiHeaders.entries()),
+        body: JSON.stringify(payload),
+        method: 'POST',
+        credentials: 'same-origin'
+    })
+    
+    const responseClone = request.clone()
+    
+    const {status, message} = await responseClone.json()    
+
+    // console.log(status, message);
+    
+    if (status){     
+        
+        return {
+            status: true,
+            message: "Transaction successful",
+        }
+    }else{
+        return {
+            status: false,
+            message: message,
+            cookies: null
+        }
+    }
+
+
+}
+
+
