1927adcca2b59045621bffdb96f26f701ce4e124
Loluwafemi <abeloluwafemishokefun@gmail.com>
2025-10-23 19:01:39 +0100

    receipt machine origin api insertion

---
+++ app/backends/sveltekit/src/routes/api/service/scanner/+server.ts
@@ -0,0 +1,32 @@
+import { RECEIPT_SCANNER_DOMAIN } from '$env/static/private'
+import { REQUESTAUTHENTICATOR } from '$lib/index.server';
+import type { RequestHandler } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
+
+
+/* 
+Broadcast the env.RECEIPT_SCANNER_DOMAIN
+*/
+
+export const GET: RequestHandler = async (event) => {
+
+    REQUESTAUTHENTICATOR(event)
+
+    /* 
+    check if the origin is active by running a surface get request
+    */
+    const response = await fetch(RECEIPT_SCANNER_DOMAIN, {
+        method: "GET",
+        redirect: 'follow'
+    }).then(async (response)=> {
+        if (response.ok) {
+            return json({domain: RECEIPT_SCANNER_DOMAIN, status: true});
+        }else{
+            return json({domain: RECEIPT_SCANNER_DOMAIN, status: false});
+        }
+    }).catch((e)=> {
+            return json({domain: RECEIPT_SCANNER_DOMAIN, status: false, message: e});
+    })
+    
+    return response
+};
